f08870069fb7b1371802355ddb7d0faf5596ef88
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-01-05 15:06:18 -0500

    Hide filter if empty; problem with filter DOM els

--- script.js
+++ script.js
@@ -12,15 +12,21 @@ Table of Contents
 /* ########DOM Variables######## */
 const filter = document.querySelector(".filter");
 const filterItemContainer = document.querySelector(".filter__item__container");
-const filterRemove = document.querySelector(".filter__item__remove");
+let filterRemove = document.querySelectorAll(".filter__item__remove");
 const filterClear = document.querySelector(".filter__clear");
 const itemContainer = document.querySelectorAll(".item__container");
 const itemFilter = document.querySelectorAll(".item__filter");
 
 /* ########Filter######## */
+document.addEventListener("mouseup", () => {
+  if (!filterItemContainer.hasChildNodes()) filter.classList.add("hidden");
+});
+
 // Create filter item when item filter clicked
 itemFilter.forEach((filterChoice) => {
   filterChoice.addEventListener("click", () => {
+    // Remove hidden filter if empty
+    if (filter.classList.contains("hidden")) filter.classList.remove("hidden");
     // Check filter if choice already selected
 
     const newFilterItem = document.createElement("button");
@@ -30,13 +36,36 @@ itemFilter.forEach((filterChoice) => {
     newFilterName.textContent = filterChoice.textContent;
     newFilterItem.classList.add("filter__item");
     newFilterName.classList.add("filter__item__name");
+    newFilterItem.setAttribute("id", filterChoice.textContent);
     newFilterRemove.classList.add("filter__item__remove");
     newFilterRemove.src = "images/icon-remove.svg";
 
     filterItemContainer.appendChild(newFilterItem);
     newFilterItem.appendChild(newFilterName);
     newFilterItem.appendChild(newFilterRemove);
+
+    filterRemove = document.querySelectorAll(".filter__item__remove");
+    console.log(filterRemove);
   });
 });
+
+// Remove filter items when clicked
+filterRemove.forEach((item) => {
+  item.addEventListener("mousedown", (e) => {
+    console.log(e);
+    console.log(filterRemove);
+    // item.parentNode.parentNode.removeChild(document.getElementById());
+  });
+});
+
+document.addEventListener("keypress", () => {
+  console.log(filterRemove);
+});
+
+for (let i = 0; i < filterRemove.length; i++) {
+  filterRemove[i].addEventListener("click", () => {
+    console.log("hey ", i);
+  });
+}
 
 /* ########Items######## */
